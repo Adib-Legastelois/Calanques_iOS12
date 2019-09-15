c04db75686db95adfdca8f261d04f8ea58dca445
Adib Legastelois <adib.legastelois@icloud.com>
2019-09-15 17:58:08 +0100

    Ajout des Nouveaux Controllers

---
+++ Les Calanques/Controler/ControllerAvecCarte.swift
@@ -0,0 +1,30 @@
+//
+//  ControllerAvecCarte.swift
+//  Les Calanques
+//
+//  Created by Adib Lgs on 2019-09-15.
+//  Copyright © 2019 Adib Lgs. All rights reserved.
+//
+
+import UIKit
+
+class ControllerAvecCarte: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
