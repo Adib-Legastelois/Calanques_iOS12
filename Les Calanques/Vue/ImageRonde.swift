b0315691df0b51bb2987e1e118e5ca482ccd6de6
Adib Legastelois <adib.legastelois@icloud.com>
2019-09-15 19:40:50 +0100

    Custom TableViewCell

---
+++ Les Calanques/Vue/ImageRonde.swift
@@ -0,0 +1,32 @@
+//
+//  ImageRonde.swift
+//  Les Calanques
+//
+//  Created by Adib Lgs on 2019-09-15.
+//  Copyright © 2019 Adib Lgs. All rights reserved.
+//
+
+import UIKit
+
+class ImageRonde: UIImageView {
+   
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+   
+    }
+    
+    func setup() {
+        layer.cornerRadius = frame.height / 2
+        layer.borderColor = UIColor.white.cgColor
+        layer.borderWidth = 2
+        clipsToBounds = true
+        contentMode = .scaleAspectFill
+    }
+    
+}
