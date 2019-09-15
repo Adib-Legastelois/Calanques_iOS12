0cf02fc01074cb504e5eee1b8257a81a95c5cc4d
Adib Legastelois <adib.legastelois@icloud.com>
2019-09-15 16:35:23 +0100

    Initial Commit

---
+++ Les Calanques/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Les Calanques
+//
+//  Created by Adib Lgs on 2019-09-15.
+//  Copyright © 2019 Adib Lgs. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
